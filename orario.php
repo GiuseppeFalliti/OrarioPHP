26f272f483179010eedc971f65ebbb76a08afee4
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2024-10-01 09:01:39 +0200

    Create orario.php

---
+++ orario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="generator" content="AlterVista - Editor HTML"/>
+  <title></title>
+</head>
+<body>
+<h1 style=" justify-content: center; text-align:center;  align-items: center; margin-top: 400px;">
+<?php
+if(date("H")<=18){
+echo "Buongiorno ";
+} else echo "Buonasera ";
+
+echo date("Y-m-d H:i:s");
+?>
+</h1>
+</body>
+</html>
